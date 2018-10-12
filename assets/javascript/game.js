3d25dc844f4bc5ebb56d4ffb91a1a9fdd97ec25c
Koxinga <timwu1015@gmail.com>
2018-10-12 16:41:16 -0400

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,62 +4,74 @@ var wordBase = ["trump", "tradewar", "dataprivacy", "denuclearization", "bullmar
 
 var wins = 0;
 
+var n = 0;
+
+var remainder = 15;
+
+var mirror = [];
+
+var trashbin = [];
+
+var pinpoint = [];
+
+var keyword = wordBase[n];
+
+for (var j = 0; j< keyword.length; j++) {
+    mirror[j] = "-";
+}
+
+$("#keyword").text(mirror.join(""));
+$("#guess-remaining").text(remainder);
+$("#win-number").text(wins);
+
 function locations(substring,string){
     var a=[],i=-1;
     while((i=string.indexOf(substring,i+1)) >= 0) a.push(i);
     return a;
-  }
+}
 
-
-$("#win-number").text(wins);
-
-$("#start-game").on("click", function() {
-
- for (var n = 0; n < wordBase.length; n++) {
-
-    var keyword = wordBase[n];
-
-    var remainder = 15;
-
-    $("#guess-remaining").text(remainder);
-
-    var mirror = [];
-
-    var trashbin = [];
-
-    var pinpoint = []
-
+function reset(){
+    remainder = 15;
+    trashbin = [];
+    n++;
+    keyword = wordBase [n];
     for (var j = 0; j< keyword.length; j++) {
         mirror[j] = "-";
     }
-
     $("#keyword").text(mirror.join(""));
+    $("#guess-remaining").text(remainder);
+    $("#already-guessed").text(trashbin);
+}
 
-    while (remainder > 0 && mirror !== keyword.split("")) {
-        document.onkeyup = function(event) {
-            if (keyword.includes(event.key.toLowerCase()) == true) {
-                pinpoint = locations(event.key.toLocaleLowerCase(),keyword);
-                for (var k = 0; k < pinpoint.length; k++) {
-                    mirror[pinpoint[k]] = event.key.toLowerCase();
-                }
-                $("#keyword").text(mirror.join(""));
+document.onkeyup = function(event) {
+    if((remainder > 0 && mirror.join("") !== keyword)) {
+        if (keyword.includes(event.key.toLowerCase()) == true) {
+            pinpoint = locations(event.key.toLocaleLowerCase(),keyword);
+            for (var k = 0; k < pinpoint.length; k++) {
+                mirror[pinpoint[k]] = event.key.toLowerCase();
             }
-            else if (trashbin.toString().includes(event.key.toLocaleLowerCase()) == false) {
-                trashbin.push(event.key.toLowerCase());
-                $("#already-guessed").text(trashbin.join(""));
-                remainder--;
-                $("#guess-remaining").text(remainder);
+            $("#keyword").text(mirror.join(""));
             }
-            else {
-                //do nothing;
+        else if (trashbin.toString().includes(event.key.toLocaleLowerCase()) == false) {
+            trashbin.push(event.key.toLowerCase());
+            $("#already-guessed").text(trashbin.join(""));
+            remainder--;
+            $("#guess-remaining").text(remainder);
             }
-        }
-    } 
+        else {
+            //do nothing;
+             }
+    }
+    else if (mirror.join("") === keyword) {
+        $("#result").text(keyword);
+        wins++;
+        $("#win-number").text(wins);
+        reset();
+    }
+    else {
+        $("#result").text(keyword);
+        reset();
+    }
     
-    $("#result").text(keyword);
-
- }
-
-});
-
+}  
 });
